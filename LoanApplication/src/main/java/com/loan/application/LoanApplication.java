b4e767db87e5fc1db43e2af691708e9cb339858e
nikhil97agr <nikhil97agrawal@gmail.com>
2021-04-28 18:58:13 +0530

    changes

--- LoanApplication/src/main/java/com/loan/application/LoanApplication.java
+++ LoanApplication/src/main/java/com/loan/application/LoanApplication.java
@@ -2,14 +2,31 @@ package com.loan.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class LoanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanApplication.class, args);
-		
+	}
+
+	@Bean
+	public WebMvcConfigurer getCorsConfiguration() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOriginPatterns("*")
+						.allowedMethods("*")
+						.allowedHeaders("*");
+			}
+			
+		};
 	}
 
 }
-	
